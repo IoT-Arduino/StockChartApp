b34236e5af39069030cf62ed8a868417d2c0311d
IoT-Arduino <bunkajin@icloud.com>
2022-05-03 16:11:01 +0900

    トップページ　ETF説明追加

--- pages/index.tsx
+++ pages/index.tsx
@@ -66,60 +66,192 @@ const Home: NextPage = ({ fundsData }: any) => {
         <div>
           <p>VOO,VTI,VT</p>
           <EtfCompareLineChart fundsData={fundsData} />
-          <p>VOOの説明</p>
-          <p>VTIの説明</p>
-          <p>VTの説明</p>
-          <p>主要構成銘柄（VOO,VTI,VT共通）</p>
+          <p>VOOの説明：S&P500インデックス指数に連動したETF</p>
+          <p>VTIの説明：中小型株を含めた米国市場の約4,000銘柄をカバーしているETF。厚切りジェイソンさんが著書「ジェイソン流お金の増やし方」でおすすめしている。</p>
+          <p>VTの説明：先進国と新興国市場の両方を対象とし、米国内外の株式で構成されるETF。「ほったらかし投資術」で推奨されている。</p>
+          <p>上位構成銘柄（VOO,VTI,VT共通）</p>
           <ul>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
+            <li>
+              <Link href="/stocks/AAPL">
+                <a>AAPL:アップル</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/stocks/MSFT">
+                <a>MSFT:マイクロソフト</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/stocks/AMZN">
+                <a>AMZN:アマゾン</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/stocks/GOOGL">
+                <a>GOOGL:アルファベット</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/stocks/TSLA">
+                <a>TSLA:テスラ</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/stocks/FB">
+                <a>FB:メタ</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/stocks/NVDA">
+                <a>NVDA:エヌビディア</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/stocks/BRKB">
+                <a>BRKB:バークシャーハサウェイ</a>
+              </Link>
+            </li>
           </ul>
         </div>
 
         <div>
           <p>{ fundsData.vigData.meta.symbol}</p> 
           <EtfCandleChart etfData={fundsData.vigData} />
-          <p>ETFの説明、構成銘柄</p>
+          <p>VIGの説明：、10年以上連続で一貫して増配する方針がとられている米国株（約250銘柄）を投資対象とするETFです（REITを除く）。</p>
+          <p>上位構成銘柄</p>
           <ul>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
+            <li>
+              <Link href="/stocks/MSFT">
+                <a>MSFT:マイクロソフト</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/stocks/UNH">
+                <a>UNH:ユナイテッドヘルス・グループ</a>
+              </Link>
+            </li>
+
+            <li>
+              <Link href="/stocks/JNJ">
+                <a>JNJ:ジョンソン・エンド・ジョンソン(J&J)</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/stocks/JPM">
+                <a>JPモルガン・チェース・アンド・カンパニー</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/stocks/PG">
+                <a>PG:プロクター・アンド・ギャンブル(P&G)</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/stocks/V">
+                <a>V:ビザ</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/stocks/HD">
+                <a>HD:ホーム・デポ</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/stocks/MA">
+                <a>MA:マスターカード</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/stocks/AVGO">
+                <a>AVGO:ブロードコム</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/stocks/COST">
+                <a>COST:コストコホールセール</a>
+              </Link>
+            </li>
           </ul>
         </div>
 
         <div>
           <p>{ fundsData.vymData.meta.symbol}</p>
           <EtfCandleChart etfData={fundsData.vymData} />
-          <p>ETFの説明、構成銘柄</p>
+          <p>VYMの説明：全米国銘柄の中から大型株を中心に予想配当利回りが市場平均を上回る銘柄で構成されています。（FTSEハイデ ィビデンド・イールド指数に連動します）</p>
+          <p>上位構成銘柄</p>
           <ul>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
+            <li>
+              <Link href="/stocks/JNJ">
+                <a>JNJ:ジョンソン・エンド・ジョンソン(J&J)</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/stocks/JPM">
+                <a>JPモルガン・チェース・アンド・カンパニー</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/stocks/PG">
+                <a>PG:プロクター・アンド・ギャンブル(P&G)</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/stocks/XOM">
+                <a>XOM:エクソンモービル</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/stocks/CVX">
+                <a>CVX:シェブロン</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/stocks/HD">
+                <a>HD:ホームデポ</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/stocks/BAC">
+                <a>BAC:バンク・オブ・アメリカ</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/stocks/PFE">
+                <a>PFE:ファイザー</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/stocks/ABBV">
+                <a>ABBV:アッヴィ</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/stocks/AVGO">
+                <a>AVGO:ブロードコム</a>
+              </Link>
+            </li>
           </ul>
         </div>
 
         <div>
           <p>{ fundsData.vwoData.meta.symbol}</p>
           <EtfCandleChart etfData={fundsData.vwoData} />
-          <p>ETFの説明、構成銘柄</p>
+          <p>VWOの説明：FTSEエマージング・マーケッツ・インデックスに連動する投資成果を目指す。ブラジル、ロシア、インド、台湾、中国、南アフリカなど、世界中の新興国市場で大型・中型株を保有するETF</p>
+         　<p>構成銘柄</p>
           <ul>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
+            <li>2330:TT(TSM:US)/台湾積体電路製造 [TSMC/台湾セ]</li>
+            <li>700:HK/騰訊控股[テンセント・ホールディングス]</li>
+            <li>9988:HK/アリババグループ・ホールディング</li>
+            <li>RIL:IN/リライアンス・インダストリーズ</li>
+            <li>INFO:IN/インフォシス</li>
+            <li>VALE3:BZ/ヴァーレ</li>
+            <li>3690:HK/美団[メイトゥアン]</li>
+            <li>939:HK/中国建設銀行 [チャイナ・コンストラクション]</li>
           </ul>
         </div>
         
         <Link href="/stocks">
-          <a>株価一覧ページへ</a>
+          <a>株式情報一覧ページへ</a>
         </Link>
       </main>
     </div>
