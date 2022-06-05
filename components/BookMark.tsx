2e9658c8ddb98096b6f80944b6348ca90f8d574a
IoT-Arduino <bunkajin@icloud.com>
2022-06-05 20:50:59 +0900

    BookMark,Comments入力,Marker入力等スタイル調整

--- components/BookMark.tsx
+++ components/BookMark.tsx
@@ -1,16 +1,18 @@
 // <!-- 必要なものをimport -->
 import { useState, useEffect } from 'react'
 import { supabase } from '../utils/supabase'
-import { Button } from "@supabase/ui";
 
-export default function BookMark({ user, ticker }:{ user:any, ticker:string}) {
+import { Button } from '@mantine/core'
+import { useToggle } from '@mantine/hooks'
+import * as AiIcons from 'react-icons/ai'
 
+export default function BookMark({ user, ticker }: { user: any; ticker: string }) {
   //  <!-- BookMark -->
-  const [star, setStar] = useState<boolean|null>(false)
+  const [star, setStar] = useState<boolean | null>(false)
   const [errorText, setError] = useState('')
 
   useEffect(() => {
-      fetchBookMark()
+    fetchBookMark()
   }, [])
 
   // <!-- supabaseに接続 -->  // user_id とtickerでマッチさせる。
@@ -18,49 +20,57 @@ export default function BookMark({ user, ticker }:{ user:any, ticker:string}) {
     let { data: items, error } = await supabase
       .from('bookmark')
       .select('*')
-      .match({ticker: ticker, user_id: user.id})
-    
+      .match({ ticker: ticker, user_id: user.id })
+
     if (error) console.log('error', error)
     else {
-      if(items){
-        setStar(items.bookmark)
+      if (items) {
+        setStar(true)
       }
     }
   }
 
-   const toggleStar = async () => {   
-     if (!star) {
-       let { data, error } = await supabase
-          .from('bookmark')
-          .insert({ bookmark: true, user_id: user.id, ticker})
+  const toggleStar = async () => {
+    if (!star) {
+      let { data, error } = await supabase
+        .from('bookmark')
+        .insert({ bookmark: true, user_id: user.id, ticker })
 
-        if (error) setError(error.message)
-        else {
-          setStar(true)
-        }
-     } else {
-       let { data, error } = await supabase.from('bookmark').delete().match({ticker: ticker, user_id: user.id})
-        if (error) setError(error.message)
-        else {
-          setStar(false)
-        }
-     }
+      if (error) setError(error.message)
+      else {
+        setStar(true)
+      }
+    } else {
+      let { data, error } = await supabase
+        .from('bookmark')
+        .delete()
+        .match({ ticker: ticker, user_id: user.id })
+      if (error) setError(error.message)
+      else {
+        setStar(false)
+      }
     }
+  }
 
   return (
     <div>
       {/* {!!errorText && <Alert text={errorText} />} */}
-        <Button block
-          onClick={(e) => {
-            e.preventDefault()
-            e.stopPropagation()
-            toggleStar()
-          }}
-          className="h-4 ml-2 border-2 hover:border-black rounded"
-        >
-          {star ? 'BookMark ★' : 'BookMark ×'}
-        </Button>
+
+      <div
+        onClick={(e) => {
+          e.preventDefault()
+          e.stopPropagation()
+          toggleStar()
+        }}
+        className='ml-2 rounded border-2 hover:border-black p-2 cursor-pointer text-green-600'
+      > 
+        {star ? (
+           <AiIcons.AiTwotoneStar size={16} className='text-green-600 align-middle'/>
+        ) : (
+          <AiIcons.AiOutlineStar size={16} className='text-green-600 align-middle'/>
+        )}
+        <span className="ml-1 text-sm">BookMark</span> 
+      </div>
     </div>
   )
 }
-
