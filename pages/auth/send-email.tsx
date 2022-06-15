ead2f1a6e6ddcd7855bc8f55f00dc335929d041e
IoT-Arduino <bunkajin@icloud.com>
2022-06-15 10:19:18 +0900

    auth input系のスタイル修正

--- pages/auth/send-email.tsx
+++ pages/auth/send-email.tsx
@@ -2,7 +2,7 @@
 import { useState } from 'react';
 import { NextPage } from 'next';
 import { supabase } from './../../utils/supabase';
-
+import style from './auth.module.css'
 
 const SendEmailToResetPassword: NextPage = () => {
     const [email, setEmail] = useState<string>('');
@@ -42,7 +42,7 @@ const SendEmailToResetPassword: NextPage = () => {
     return (
       <div>
        <p>登録されているメールアドレスを入力してください</p>
-       <form onSubmit={handleSubmitEmail}>
+       <form onSubmit={handleSubmitEmail} className={style.inputField}>
           <input value={email} type="email" onChange={handleSetEmail} placeholder="メールアドレス" />
           <button type="submit">送信</button>
         </form>
