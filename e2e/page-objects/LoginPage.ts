1af33cd78097d1cff4bf70340865c627f27b5904
IoT-Arduino <bunkajin@icloud.com>
2022-07-16 14:16:13 +0900

    LoginPage : page-objext 作成

---
+++ e2e/page-objects/LoginPage.ts
@@ -0,0 +1,31 @@
+import { expect, Locator, Page } from '@playwright/test'
+
+export class LoginPage  {
+  // Define Selectors
+  readonly page: Page
+  readonly emailInput: Locator
+  readonly passwordInput: Locator
+  readonly submitButton: Locator
+//   readonly errorMessage: Locator
+
+  // Init Selectors
+  constructor(page: Page) {
+    this.page = page
+    this.emailInput = page.locator('input[type="email"]')
+    this.passwordInput = page.locator('input[type="password"]')
+    this.submitButton = page.locator('data-testid=login-submit')
+    // this.errorMessage = page.locator('.alert-error')
+  }
+
+  // Define login page methods
+  async login(email: string, password: string) {
+    await this.emailInput.type(email)
+    await this.passwordInput.type(password)
+    await this.submitButton.click()
+  }
+
+//   async asserErrorMessage() {
+//     await expect(this.errorMessage).toContainText('Invalid Login')
+//   }
+
+}
