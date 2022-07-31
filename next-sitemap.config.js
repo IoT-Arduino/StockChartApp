d4d99ba71fb99428e5ae9d363acae4be67abf6b8
IoT-Arduino <bunkajin@icloud.com>
2022-07-31 20:11:36 +0900

    sitemap config修正

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,14 +1,15 @@
 module.exports = {
   siteUrl: process.env.NEXT_PUBLIC_API_ENDOPOINT,
   generateRobotsTxt: true,
+  robotsTxtOptions: {
+    policies: [
+      { userAgent: '*', disallow: '/auth' },
+      { userAgent: '*', disallow: '/stocks/candidate' },
+      { userAgent: '*', disallow: '/member' },
+      { useAgent: '*', allow: '/' },
+    ],
+  },
   sitemapSize: 7000,
-  exclude: [
-    '/stocks/candidate',
-    '/auth/reset-password',
-    '/auth/send-email',
-    '/auth/signin',
-    '/auth/signup',
-    '/member',
-  ],
+  exclude: ['/stocks/candidate', '/auth', '/member'],
   // outDir: './out',
 }
