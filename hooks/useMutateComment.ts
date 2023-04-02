5200896402a55f4b3ba73e21bedc9ee7f3c15557
IoT-Arduino <bunkajin@icloud.com>
2023-04-02 20:04:07 +0900

    to typescript : useMutateComments

--- hooks/useMutateComment.ts
+++ hooks/useMutateComment.ts
@@ -1,7 +1,13 @@
-import { useQueryClient, useMutation } from 'react-query'
+import { useQueryClient, useMutation} from 'react-query'
 import useStore from './../store/store'
 
 import { supabase } from './../utils/supabase'
+
+type Comment = {
+  id: number
+  memo: string
+  date: Date
+}
 
 export const useMutateComment = () => {
   const queryClient = useQueryClient()
@@ -10,44 +16,41 @@ export const useMutateComment = () => {
   // const user = supabase.auth.user()
 
   const createCommentMutation = useMutation(
-    async (comment) => {
-      const { data, error } = await supabase.from('comments').insert(comment)
+    async (comment: Partial<Comment>[] | Partial<Comment>) => {
+      const { data, error } = await supabase.from<Comment>('comments').insert(comment)
       if (error) throw new Error(error.message)
-      return data
+      return data![0]
     },
     {
       onSuccess: (res) => {
-        const previousComments = queryClient.getQueryData('comments')
+        const previousComments = queryClient.getQueryData<Comment[]>('comments')
         if (previousComments) {
-          queryClient.setQueryData('comments', [...previousComments, res[0]])
+          queryClient.setQueryData('comments', [...previousComments, res])
         }
         reset()
       },
-      onError: (err) => {
+      onError: (err: any) => {
         alert(err.message)
         reset()
       },
     }
   )
-  const updateCommentMutation = useMutation(
+  const updateCommentMutation = useMutation<Comment, Error, Comment>(
     async (comment) => {
-      console.log(comment)
       const { data, error } = await supabase
-        .from('comments')
-        .update({ memo: comment.memo,date:comment.date })
+        .from<Comment>('comments')
+        .update({ memo: comment.memo, date: comment.date })
         .eq('id', comment.id)
       if (error) throw new Error(error.message)
-      return data
+      return data![0]
     },
     {
       onSuccess: (res, variables) => {
-        const previousComments = queryClient.getQueryData('comments')
+        const previousComments = queryClient.getQueryData<Comment[]>('comments')
         if (previousComments) {
           queryClient.setQueryData(
             'comments',
-            previousComments.map((comment) =>
-              comment.id === variables.id ? res[0] : comment
-            )
+            previousComments.map((comment) => (comment.id === variables.id ? res: comment))
           )
         }
         reset()
@@ -58,7 +61,7 @@ export const useMutateComment = () => {
       },
     }
   )
-  const deleteCommentMutation = useMutation(
+  const deleteCommentMutation = useMutation<unknown, Error, number>(
     async (id) => {
       const { data, error } = await supabase.from('comments').delete().eq('id', id)
       if (error) throw new Error(error.message)
@@ -66,7 +69,7 @@ export const useMutateComment = () => {
     },
     {
       onSuccess: (_, variables) => {
-        const previousComments = queryClient.getQueryData('comments')
+        const previousComments = queryClient.getQueryData<Comment[]>('comments')
         if (previousComments) {
           queryClient.setQueryData(
             'comments',
