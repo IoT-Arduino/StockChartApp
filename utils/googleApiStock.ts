9c8b562446fce06c93777dd84a0844552ad988e7
IoT-Arduino <bunkajin@icloud.com>
2023-04-07 10:43:01 +0900

    api , util , auth 関連 TypeScript化

--- utils/googleApiStock.ts
+++ utils/googleApiStock.ts
@@ -1,15 +1,30 @@
 import { google } from 'googleapis'
 import dayjs from 'dayjs'
 
-export async function getStockInfo(id) {
+interface NewsData {
+  date: string
+  news: string
+}
+
+interface InfoData {
+  date: string
+  info: string
+}
+
+interface Data {
+  newsDataFiltered: NewsData[] | null
+  infoDataFiltered: InfoData[] | null
+}
+
+export async function getStockInfo(id: string): Promise<any> {
   try {
     const scopes = ['https://www.googleapis.com/auth/spreadsheets.readonly']
     const jwt = new google.auth.JWT(
       process.env.GOOGLE_SHEETS_CLIENT_EMAIL,
-      null,
+      undefined,
       // we need to replace the escaped newline characters
       // https://stackoverflow.com/questions/50299329/node-js-firebase-service-account-private-key-wont-parse
-      process.env.GOOGLE_SHEETS_PRIVATE_KEY.replace(/\\n/g, '\n'),
+      process.env.GOOGLE_SHEETS_PRIVATE_KEY!.replace(/\\n/g, '\n') ,
       scopes
     )
 
@@ -20,7 +35,7 @@ export async function getStockInfo(id) {
     })
 
     const googleSheetData = response.data.values
-    const filteredSheetData = googleSheetData.filter((item) => {
+    const filteredSheetData = googleSheetData?.filter((item) => {
       return item[0] == id
     })
 
@@ -29,22 +44,32 @@ export async function getStockInfo(id) {
     var DATES_OFFSET = 70 * 365 + 17 + 1 + 1 //「1900/1/0」～「1970/1/1」 (日数)
     var MILLIS_DIFFERENCE = 9 * 60 * 60 * 1000 //UTCとJSTの時差 (ミリ秒)
 
-    function convertSn2Ut(serialNumber) {
-      // シリアル値→UNIX時間(ミリ秒)
+    // function convertSn2Ut(serialNumber: number): number {
+    //   // シリアル値→UNIX時間(ミリ秒)
+    //   return (serialNumber - DATES_OFFSET) * COEFFICIENT - MILLIS_DIFFERENCE
+    // }
+
+    const convertSn2Ut = (serialNumber: number): number => {  // シリアル値→UNIX時間(ミリ秒)
       return (serialNumber - DATES_OFFSET) * COEFFICIENT - MILLIS_DIFFERENCE
     }
-    function dateFromSn(serialNumber) {
-      // シリアル値→Date
+
+    // function dateFromSn(serialNumber: number): Date {
+    //   // シリアル値→Date
+    //   return new Date(convertSn2Ut(serialNumber))
+    // }
+
+    const dateFromSn = (serialNumber: number): Date => {  // シリアル値→Date  
       return new Date(convertSn2Ut(serialNumber))
     }
 
-    let newsDataFiltered
-    let infoDataFiltered
+
+    let newsDataFiltered: NewsData[] | null
+    let infoDataFiltered: InfoData[] | null
 
     if (filteredSheetData && filteredSheetData[0][1]) {
       const news = filteredSheetData[0][1].split(',')
 
-      const newsData = news.map((item) => {
+      const newsData = news.map((item: any) => {
         const splited = item.split('-')
         const date = dateFromSn(splited[0])
         return {
@@ -53,10 +78,9 @@ export async function getStockInfo(id) {
         }
       })
 
-      newsDataFiltered = newsData.filter((item) => {
+      newsDataFiltered = newsData.filter((item: any) => {
         return item.news !== undefined
       })
-
     } else {
       newsDataFiltered = null
     }
@@ -64,7 +88,7 @@ export async function getStockInfo(id) {
     if (filteredSheetData && filteredSheetData[0][2]) {
       const info = filteredSheetData[0][2].split(',')
 
-      const infoData = info.map((item) => {
+      const infoData = info.map((item: any) => {
         const splited = item.split('-')
         const date = dateFromSn(splited[0])
         return {
@@ -73,22 +97,19 @@ export async function getStockInfo(id) {
         }
       })
 
-      infoDataFiltered = infoData.filter((item) => {
+      infoDataFiltered = infoData.filter((item: any) => {
         return item.news !== undefined
       })
-
-
     } else {
       infoDataFiltered = null
     }
 
     const data = {
       newsDataFiltered,
-      infoDataFiltered
+      infoDataFiltered,
     }
 
     return data
-
   } catch (err) {
     console.log(err)
   }
