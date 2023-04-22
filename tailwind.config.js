12b7a09754b1326c2956181ba5d2cfcc8c926d39
IoT-Arduino <bunkajin@icloud.com>
2023-04-22 17:10:37 +0900

    react-datepicker install and configure

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,20 @@
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-    "./locales/**/*.{js,ts,jsx,tsx}",
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    './locales/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundColor: {
+        datepicker: '#f0f0f0',
+        'datepicker-selected': '#3f51b5',
+        'datepicker-hover': '#9fa8da',
+      },
+      textColor: {
+        'datepicker-selected': '#ffffff',
+      },
+    },
   },
   plugins: [],
 }
