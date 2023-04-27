2f7cf38dc9ba8155fd9d4aadb219d7f254435249
IoT-Arduino <bunkajin@icloud.com>
2023-04-27 16:12:21 +0900

    minor fix for InputComment

--- functions/CustomCalendarHeader.tsx
+++ functions/CustomCalendarHeader.tsx
@@ -10,9 +10,10 @@ interface CustomCalendarHeaderProps {
   increaseMonth: () => void
   prevMonthButtonDisabled: boolean
   nextMonthButtonDisabled: boolean
+  locale?: 'ja-JP' | 'en-US'
 }
 
-const YEARS_FOR_DROPDOWN = 15
+const YEARS_FOR_DROPDOWN = 10
 
 export const CustomCalendarHeader: React.FC<CustomCalendarHeaderProps> = ({
   date,
@@ -22,12 +23,52 @@ export const CustomCalendarHeader: React.FC<CustomCalendarHeaderProps> = ({
   increaseMonth,
   prevMonthButtonDisabled,
   nextMonthButtonDisabled,
+  locale
 }) => {
   const years = [...Array(YEARS_FOR_DROPDOWN)].map((_, i) => new Date().getFullYear() - i)
 
+    const MonthSelect = ({style}) => {
+      console.log(style)
+    return (
+        <select
+          value={date.getMonth()}
+          onChange={({ target: { value } }: ChangeEvent<HTMLSelectElement>) =>
+            changeMonth(Number(value))
+          }
+          className={`react-datepicker__month-select ${style}`}
+        >
+          {Array.from({ length: 12 }, (_, index) =>
+            new Date(date.getFullYear(), index).toLocaleDateString(locale, { month: 'long' })
+          ).map((month, index) => (
+            <option key={month} value={index}>
+              {month}
+            </option>
+          ))}
+        </select>
+    )
+  }
+
+  const YearSelect = ({style}) => {
+    return (
+        <select
+          value={date.getFullYear()}
+          onChange={({ target: { value } }: ChangeEvent<HTMLSelectElement>) =>
+            changeYear(Number(value))
+          }
+          className={`react-datepicker__year-select ${style}`}
+        >
+          {years.map((year) => (
+            <option key={year} value={year}>
+              {year}
+            </option>
+          ))}
+        </select>
+    )
+  }
+
   return (
     <div className='react-datepicker__header'>
-      <div>
+      <div className='mb-2'>
         <button
           className='react-datepicker__navigation react-datepicker__navigation--previous'
           onClick={decreaseMonth}
@@ -36,7 +77,7 @@ export const CustomCalendarHeader: React.FC<CustomCalendarHeaderProps> = ({
           <MdKeyboardDoubleArrowLeft />
         </button>
         <div className='mx-2'>
-          {date.toLocaleDateString('ja-JP', { year: 'numeric', month: 'long' })}
+          {date.toLocaleDateString(locale, { year: 'numeric', month: 'long' })}
         </div>
         <button
           className='react-datepicker__navigation react-datepicker__navigation--next'
@@ -53,34 +94,19 @@ export const CustomCalendarHeader: React.FC<CustomCalendarHeaderProps> = ({
           onClick={decreaseMonth}
           disabled={prevMonthButtonDisabled}
         />
-        <select
-          value={date.getFullYear()}
-          onChange={({ target: { value } }: ChangeEvent<HTMLSelectElement>) =>
-            changeYear(Number(value))
-          }
-          className='react-datepicker__year-select mr-2 mt-2'
-        >
-          {years.map((year) => (
-            <option key={year} value={year}>
-              {year}
-            </option>
-          ))}
-        </select>
-        <select
-          value={date.getMonth()}
-          onChange={({ target: { value } }: ChangeEvent<HTMLSelectElement>) =>
-            changeMonth(Number(value))
-          }
-          className='react-datepicker__month-select'
-        >
-          {Array.from({ length: 12 }, (_, index) =>
-            new Date(date.getFullYear(), index).toLocaleDateString('ja-JP', { month: 'long' })
-          ).map((month, index) => (
-            <option key={month} value={index}>
-              {month}
-            </option>
-          ))}
-        </select>
+        {locale === 'ja-JP' ? (
+          <>
+            <YearSelect style={'mr-2'}/>
+            <MonthSelect />
+          </>
+        ) : (
+          <>
+            <MonthSelect  style={'mr-2'}/>
+            <YearSelect />
+           </>
+        )
+        }
+
         <button
           className='react-datepicker__navigation react-datepicker__navigation--next'
           onClick={increaseMonth}
