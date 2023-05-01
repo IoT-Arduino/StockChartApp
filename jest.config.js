bf29ea311f2b1437f89736c84e4ed56f16d2804a
IoT-Arduino <bunkajin@icloud.com>
2023-05-01 15:54:18 +0900

    coverage report 設定

--- jest.config.js
+++ jest.config.js
@@ -18,6 +18,18 @@ const customJestConfig = {
   roots: ['<rootDir>/__tests__'],
   testEnvironment: 'jest-environment-jsdom',
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js', '<rootDir>/__mocks__/matchMedia.mock.ts'],
+  // Add coverage-related configuration
+  collectCoverage: true,
+  collectCoverageFrom: [
+    '<rootDir>/components/**/*.{js,jsx,ts,tsx}',
+    '<rootDir>/pages/**/*.{js,jsx,ts,tsx}',
+    '<rootDir>/functions/**/*.{js,jsx,ts,tsx}',
+    '<rootDir>/hooks/**/*.{js,jsx,ts,tsx}',
+    '<rootDir>/store/**/*.{js,jsx,ts,tsx}',
+    '<rootDir>/utils/**/*.{js,jsx,ts,tsx}',
+  ],
+  coverageReporters: ['lcov', 'text'],
+  coverageDirectory: '<rootDir>/coverage',
 }
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
