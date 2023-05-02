359941dfbc2218fc0996557e88d071834df68446
IoT-Arduino <bunkajin@icloud.com>
2023-05-02 11:20:39 +0900

    minro fix

--- store/store.ts
+++ store/store.ts
@@ -1,7 +1,5 @@
-
-
 import { configureStore } from '@reduxjs/toolkit'
-import rootReducer, { RootState } from './rootReducer'
+import rootReducer from './rootReducer'
 
 const store = configureStore({
   reducer: rootReducer,
@@ -10,46 +8,4 @@ const store = configureStore({
 export type AppDispatch = typeof store.dispatch
 export default store
 
-// import { create } from 'zustand'
 
-// // Types
-// import { EditedMarker, EditedComments } from '../types/StoreTypes'
-
-// type State = {
-//   editedComment: EditedComments
-//   editedMarker: EditedMarker
-//   updateEditedComment: (payload: EditedComments) => void
-//   updateEditedMarker: (payload: EditedMarker) => void
-//   resetEditedComment: () => void
-//   resetEditedMarker: () => void
-// }
-
-// const useStore = create<State>((set) => ({
-//   // Comment
-//   editedComment: { ticker: '', date: '', memo: '', id: null },
-//   updateEditedComment: (payload) =>
-//     set({
-//       editedComment: {
-//         ticker: payload.ticker,
-//         date: payload.date,
-//         memo: payload.memo,
-//         id: payload.id,
-//       },
-//     }),
-//   resetEditedComment: () => set({ editedComment: { ticker: '', date: '', memo: '', id: null  } }),
-
-//   // Marker
-//   editedMarker: { ticker: '', date: '', memo: '', id: null },
-//   updateEditedMarker: (payload) =>
-//     set({
-//       editedMarker: {
-//         ticker: payload.ticker,
-//         date: payload.date,
-//         memo: payload.memo,
-//         id: payload.id,
-//       },
-//     }),
-//   resetEditedMarker: () => set({ editedMarker: { ticker: '', date: '', memo: '' , id: null } }),
-// }))
-
-// export default useStore
