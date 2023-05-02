989a2f2a4fbf80142a9ed61e1f65fb799a8b0f97
IoT-Arduino <bunkajin@icloud.com>
2023-05-02 12:25:38 +0900

    Comment,Marker入力のテスト作成

---
+++ __tests__/components/InpurCommentsState.test.tsx
@@ -0,0 +1,114 @@
+import { render, fireEvent, waitFor, screen } from '@testing-library/react'
+// import { act, renderHook } from '@testing-library/react-hooks'
+import userEvent from '@testing-library/user-event'
+import { Provider } from 'react-redux'
+import store from '../../store/store'
+import InputComment from '../../components/InputCommentsState'
+import { QueryClient, QueryClientProvider } from 'react-query'
+import { useMutateComment } from '../../hooks/useMutateComment'
+import ja from '../../locales/ja/ja'
+
+const queryClient = new QueryClient()
+
+jest.mock('../../hooks/useQueryComments', () => ({
+  useQueryComment: () => ({
+    data: [],
+    status: 'success',
+  }),
+}))
+
+jest.mock('../../hooks/useMutateComment', () => {
+  const mockData = [{ id: 1, memo: 'Test Memo', date: '2023-05-01', ticker: 'AAPL' }]
+
+  const createCommentMutation = {
+    mutate: jest.fn(),
+    onSuccess: jest.fn(),
+  }
+
+  createCommentMutation.mutate.mockImplementation((comment) => {
+    mockData.push(comment)
+
+    // onSuccess関数を呼び出す
+    createCommentMutation.onSuccess(mockData)
+  })
+
+  return {
+    useMutateComment: () => ({
+      createCommentMutation,
+      updateCommentMutation: {
+        mutate: jest.fn(),
+      },
+      deleteCommentMutation: {
+        mutate: jest.fn(),
+      },
+    }),
+  }
+})
+
+jest.mock('../../hooks/useQueryComments', () => ({
+  useQueryComments: () => ({
+    data: [{ id: 1, memo: 'Test Memo', date: '2023-05-01', ticker: 'AAPL' }],
+    error: null,
+    isLoading: false,
+    isSuccess: true,
+    isError: false,
+  }),
+}))
+
+jest.mock('next/router', () => ({
+  useRouter: () => ({
+    locale: 'ja-JP',
+  }),
+}))
+
+const setup = () => {
+  return render(
+    <QueryClientProvider client={queryClient}>
+      <Provider store={store}>
+        <InputComment ticker='AAPL' t={ja} />
+      </Provider>
+    </QueryClientProvider>
+  )
+}
+
+describe('InputComment component', () => {
+  it('adds a Comment correctly', async () => {
+    const { getByTestId, container } = setup()
+
+    const commentDateInput = container.querySelector('.react-datepicker__input-container input')
+    const commentMemoInput = screen.getByTestId('commentMemoInput')
+    const addCommentButton = screen.getByTestId('addComment')
+
+    const list = screen.getByRole('list')
+
+    // Get the mocked createCommentMutation
+    const { createCommentMutation } = useMutateComment()
+
+    // Select date
+    if (commentDateInput) {
+      await userEvent.type(commentDateInput, '2023/05/01')
+      expect(commentDateInput).toHaveValue('2023/05/01')
+    }
+
+    // Input memo
+    await userEvent.type(commentMemoInput, 'Test Memo')
+    expect(commentMemoInput).toHaveValue('Test Memo')
+
+    // Check that the button is enabled
+    expect(addCommentButton).not.toBeDisabled()
+
+    // Click add comment button
+    fireEvent.submit(getByTestId('inputCommentForm')) 
+
+    // Wait for the mutate function to be called
+    await waitFor(() => {
+      expect(createCommentMutation.mutate).toHaveBeenCalledTimes(1)
+    })
+
+    await waitFor(async () => {
+      const testMemoElement = await screen.findByText('Test Memo')
+      expect(testMemoElement).toBeInTheDocument()
+    })
+  })
+
+})
---
+++ __tests__/components/InputMarkerState.test.tsx
@@ -0,0 +1,114 @@
+import { render, fireEvent, waitFor, screen } from '@testing-library/react'
+// import { act, renderHook } from '@testing-library/react-hooks'
+import userEvent from '@testing-library/user-event'
+import { Provider } from 'react-redux'
+import store from '../../store/store'
+import InputMarker from '../../components/InputMarkerState'
+import { QueryClient, QueryClientProvider } from 'react-query'
+import { useMutateMarker } from '../../hooks/useMutateMarker'
+import ja from '../../locales/ja/ja'
+
+const queryClient = new QueryClient()
+
+jest.mock('../../hooks/useQueryMarker', () => ({
+  useQueryMarker: () => ({
+    data: [],
+    status: 'success',
+  }),
+}))
+
+jest.mock('../../hooks/useMutateMarker', () => {
+  const mockData = [{ id: 1, memo: 'Test Memo', date: '2023-05-01', ticker: 'AAPL' }]
+
+  const createMarkerMutation = {
+    mutate: jest.fn(),
+    onSuccess: jest.fn(),
+  }
+
+  createMarkerMutation.mutate.mockImplementation((marker) => {
+    mockData.push(marker)
+
+    // onSuccess関数を呼び出す
+    createMarkerMutation.onSuccess(mockData)
+  })
+
+  return {
+    useMutateMarker: () => ({
+      createMarkerMutation,
+      updateMarkerMutation: {
+        mutate: jest.fn(),
+      },
+      deleteMarkerMutation: {
+        mutate: jest.fn(),
+      },
+    }),
+  }
+})
+
+jest.mock('../../hooks/useQueryMarker', () => ({
+  useQueryMarker: () => ({
+    data: [{ id: 1, memo: 'Test Memo', date: '2023-05-01', ticker: 'AAPL' }],
+    error: null,
+    isLoading: false,
+    isSuccess: true,
+    isError: false,
+  }),
+}))
+
+jest.mock('next/router', () => ({
+  useRouter: () => ({
+    locale: 'ja-JP',
+  }),
+}))
+
+const setup = () => {
+  return render(
+    <QueryClientProvider client={queryClient}>
+      <Provider store={store}>
+        <InputMarker ticker='AAPL' t={ja} />
+      </Provider>
+    </QueryClientProvider>
+  )
+}
+
+describe('InputMarker component', () => {
+  it('adds a marker correctly', async () => {
+    const { getByTestId, container } = setup()
+
+    const markerDateInput = container.querySelector('.react-datepicker__input-container input')
+    const markerMemoInput = screen.getByTestId('markerMemoInput')
+    const addMarkerButton = screen.getByTestId('addMarker')
+
+    const list = screen.getByRole('list')
+
+    // Get the mocked createMarkerMutation
+    const { createMarkerMutation } = useMutateMarker()
+
+    // Select date
+    if (markerDateInput) {
+      await userEvent.type(markerDateInput, '2023/05')
+      expect(markerDateInput).toHaveValue('2023/05')
+    }
+
+    // Input memo
+    await userEvent.type(markerMemoInput, 'Test Memo')
+    expect(markerMemoInput).toHaveValue('Test Memo')
+
+    // Check that the button is enabled
+    expect(addMarkerButton).not.toBeDisabled()
+
+    // Click add marker button
+    fireEvent.submit(getByTestId('inputMarkerForm')) 
+
+    // Wait for the mutate function to be called
+    await waitFor(() => {
+      expect(createMarkerMutation.mutate).toHaveBeenCalledTimes(1)
+    })
+
+    await waitFor(async () => {
+      const testMemoElement = await screen.findByText('Test Memo')
+      expect(testMemoElement).toBeInTheDocument()
+    })
+  })
+
+})
