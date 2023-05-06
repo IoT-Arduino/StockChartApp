db76795ef0f9f814062cf75d5b0c192f3533fd2b
IoT-Arduino <bunkajin@icloud.com>
2023-05-06 19:56:33 +0900

    Playwright Config 修正

--- global-setup.js
+++
@@ -1,4 +0,0 @@
-// global-setup.js
-module.exports = async config => {
-    process.env.BASE_URL = 'https://stock-chart-app-git-develop-sa10shi.vercel.app'
-};
--- playwright.config.ts
+++ playwright.config.ts
@@ -1,29 +1,27 @@
 import { PlaywrightTestConfig, devices } from '@playwright/test'
+import dotenv from 'dotenv'
+import path from 'path'
+
+dotenv.config({ path: '.env.local' })
+
+const isProduction = process.env.NODE_ENV === 'production';
+const baseURL = isProduction ? process.env.PRODUCTION_BASE_URL : process.env.DEVELOPMENT_BASE_URL;
 
 const config: PlaywrightTestConfig = {
-  testMatch: '/e2e/**/*.spec.ts',
-  globalSetup: require.resolve('./global-setup'),
-  // webServer: {
-  //     command: 'npm run dev',
-  //     url: 'http://localhost:3000',
-  //     timeout: 120 * 1000,
-  //   },
+  testMatch: /e2e\/.*\.spec\.ts$/,
+  // globalSetup: require.resolve('./global-setup'),
   use: {
-    // baseURL: 'https://stock-chart-app-git-develop-sa10shi.vercel.app',
-    baseURL: 'http://localhost:3000',
-    headless: true,
+    baseURL: baseURL,
+    headless: false,
     ignoreHTTPSErrors: true,
     actionTimeout: 10_000,
   },
+  reporter: [['html', { open: 'always' }]],
   projects: [
     {
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
     },
-    // {
-    //     name: 'iphone12',
-    //     use: {...devices["iPhone 12"]}
-    // }
   ],
 }
 export default config
