6a7f497330ba51fdf6e66197175b8c7b1a68abe6
IoT-Arduino <bunkajin@icloud.com>
2023-05-07 09:47:45 +0900

    Change Playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -5,7 +5,7 @@ import path from 'path'
 dotenv.config({ path: '.env.local' })
 
 const isProduction = process.env.NODE_ENV === 'production';
-const baseURL = isProduction ? process.env.PRODUCTION_BASE_URL : process.env.DEVELOPMENT_BASE_URL;
+const baseURL = isProduction ? process.env.NEXT_PUBLIC_BASE_URL : process.env.DEVELOPMENT_BASE_URL
 
 const config: PlaywrightTestConfig = {
   testMatch: /e2e\/.*\.spec\.ts$/,
